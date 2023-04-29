ac62e99a94a7697b15383843b4fb31a77552aa73
Edin Šahbaz <edin.sahbaz@hotmail.com>
2023-04-29 13:28:50 +0200

    Generisanje izvjestaja

--- BingoLotteryConsole/Izvlacenje.cs
+++ BingoLotteryConsole/Izvlacenje.cs
@@ -15,20 +15,48 @@ public static class Izvlacenje
     }
 
     // ISSUE # 6
+    // Implementirati logiku za generisanje izvjestaja o dobitnim kombinacijama.
+    // Za referencu pogledati tabelu na linku: https://www.lutrijabih.ba/igre/loto-639/privremeni-izvjestaj/
     public static void GenerisiIzvjestaj(List<int[]> listici, int[] dobitnaKombinacija, int dopunskiBroj)
     {
-        // Implementirati logiku za generisanje izvjestaja o dobitnim kombinacijama.
+        // Dobitne kombinacje predstavljenje kao key value parovi
+        // Key predstavlja broj pogodaka, dok db označava dopunski broj
+        var kombinacije = new Dictionary<string, int> {
+            {"6", 0}, {"5db", 0}, {"5", 0}, {"4db", 0}, {"4", 0}, {"3db", 0}, {"3", 0}, {"2db", 0}
+        };
 
-        // Izvjestaj ispisati u formi tabele za sljedeće dobitke:
-        // - Šestica
-        // - Petica + dopunski broj
-        // - Petica
-        // - Četvorka + dopunski broj
-        // - Četvorka
-        // - Trojka + dopunski broj
-        // - Trojka
-        // - Dvojka + dopunski broj
+        // Provjera pogodaka i dopunskog broja za sve listice
+        foreach (var listic in listici)
+        {
+            int brojPogodaka = 0;
+            bool sadrziDopunskiBroj = listic.Contains(dopunskiBroj);
 
-        // Za referencu pogledati tabelu na linku: https://www.lutrijabih.ba/igre/loto-639/privremeni-izvjestaj/
+            foreach (var broj in listic)
+            {
+                if (dobitnaKombinacija.Contains(broj))
+                {
+                    brojPogodaka++;
+                }
+            }
+
+            if (brojPogodaka < 2) continue;
+            if (brojPogodaka == 2 && !sadrziDopunskiBroj) continue;
+
+            var kljuc = brojPogodaka.ToString() + (sadrziDopunskiBroj && brojPogodaka != 6 ? "db" : "");
+            kombinacije[kljuc] = kombinacije[kljuc] + 1;
+        }
+
+        // Generisanje izvjestaja
+        string izvjestaj = $"Sestice: {kombinacije["6"]}\nPetice + dopunski broj: {kombinacije["5db"]}" +
+            $"\nPetice: {kombinacije["5"]}\nCetvorke + dopunski broj: {kombinacije["4db"]}\nCetvorke: {kombinacije["4"]}" +
+            $"\nTrojke + dopunski broj: {kombinacije["3db"]}\nTrojke: {kombinacije["3"]}\nDvojke + dopunski broj: {kombinacije["2db"]}";
+
+        Console.WriteLine(izvjestaj);
+
+        // Spremanje izvjestaja u izvjestaj.txt fajl
+        using (StreamWriter writer = new StreamWriter("izvjestaj.txt"))
+        {
+            writer.WriteLine(izvjestaj);
+        }
     }
 }
